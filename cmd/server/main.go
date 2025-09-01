b5b691b68afc45b0a8677c7acfcaf31608ba8e11
enzom-uy <enzom.dev@gmail.com>
2025-09-01 13:46:11 -0300

    railway environment

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/enzom-uy/hltb-go-scraper/internal/api/routes"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("RAILWAY_ENVIRONMENT") == "" {
+		if err := godotenv.Load(); err != nil {
+			log.Println("No .env file found, using system environment variables")
+		} else {
+			log.Println("Loaded .env file for local development")
+		}
+	} else {
+		log.Println("Running in Railway - using environment variables")
 	}
+
 	routes.Setup()
 }
